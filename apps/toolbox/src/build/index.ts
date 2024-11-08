0d66cbee8897bf633640c6699ff79e7779501a0a
Shun Ueda <me@shu.nu>
2024-11-08 18:06:53 -0500

    build: exclude .cts

--- apps/toolbox/src/build/index.ts
+++ apps/toolbox/src/build/index.ts
@@ -1,5 +1,5 @@
 import { readFileSync, readdirSync } from 'node:fs'
-import { cp, writeFile } from 'node:fs/promises'
+import { cp, rm, writeFile } from 'node:fs/promises'
 import { join, relative } from 'node:path'
 import { build } from 'tsup'
 import type { PackageJson } from 'type-fest'
@@ -11,6 +11,7 @@ import { pick } from './pick'
 const cwd = process.cwd()
 const outDir = 'dist'
 
+// find all index.ts files in the src directory
 const entries = readdirSync(join(cwd, 'src'), {
   recursive: true,
   withFileTypes: true,
@@ -18,6 +19,7 @@ const entries = readdirSync(join(cwd, 'src'), {
   .filter(it => it.isFile() && it.name === 'index.ts')
   .map(it => relative(cwd, join(it.parentPath, it.name)))
 
+// build the package
 await build({
   entry: entries,
   target: compilerOptions.target || '',
@@ -34,39 +36,58 @@ await build({
   outDir,
 })
 
+// remove all .cts files (use .d.ts instead)
+await Promise.all(
+  readdirSync(join(cwd, outDir), {
+    recursive: true,
+    withFileTypes: true,
+  })
+    .filter(it => it.isFile() && it.name.endsWith('.cts'))
+    .map(it => rm(join(it.parentPath, it.name))),
+)
+
+// original package.json
+const original = readFileSync(join(cwd, 'package.json')).toString()
+
+// include only the necessary fields
+const include = pick<PackageJson>(JSON.parse(original), [
+  'name',
+  'version',
+  'type',
+  'repository',
+  'dependencies',
+  'keywords',
+])
+
+// add exports field
+const modified = {
+  ...include,
+  publishConfig: {
+    provenance: true,
+  },
+  sideEffects: false,
+  license: 'MIT',
+  exports: {
+    './package.json': './package.json',
+    ...Object.fromEntries(
+      entries.map(entry => {
+        const relative = entry.replace('src', '.')
+        return [
+          relative.replace('/index.ts', ''),
+          {
+            types: relative.replace('.ts', '.d.ts'),
+            import: relative.replace('.ts', '.js'),
+            default: relative.replace('.ts', '.cjs'),
+          },
+        ]
+      }),
+    ),
+  },
+} as PackageJson
+
+// write the package.json, LICENSE, and README.md
 await Promise.all([
-  writeFile(join(cwd, `${outDir}/LICENSE`), license(packageJson.author.name)),
   cp(join(cwd, 'README.md'), join(cwd, `${outDir}/README.md`)),
-  writeFile(
-    join(cwd, `${outDir}/package.json`),
-    JSON.stringify({
-      ...pick(
-        JSON.parse(
-          readFileSync(join(cwd, 'package.json'), 'utf-8'),
-        ) as PackageJson,
-        ['name', 'version', 'type', 'repository', 'dependencies', 'keywords'],
-      ),
-      publishConfig: {
-        provenance: true,
-      },
-      sideEffects: false,
-      license: 'MIT',
-      exports: {
-        './package.json': './package.json',
-        ...Object.fromEntries(
-          entries.map(entry => {
-            const relative = entry.replace('src', '.')
-            return [
-              relative.replace('/index.ts', ''),
-              {
-                types: relative.replace('.ts', '.d.ts'),
-                import: relative.replace('.ts', '.js'),
-                default: relative.replace('.ts', '.cjs'),
-              },
-            ]
-          }),
-        ),
-      },
-    } as PackageJson),
-  ),
+  writeFile(join(cwd, `${outDir}/LICENSE`), license(packageJson.author.name)),
+  writeFile(join(cwd, `${outDir}/package.json`), JSON.stringify(modified)),
 ])
