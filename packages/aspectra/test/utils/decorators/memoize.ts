c43e4bb268235752d9447164fcc3ad1867c9fc25
Shun Ueda <me@shu.nu>
2024-11-07 20:41:56 -0500

    refactor(dependencies): remove unused

---
+++ packages/aspectra/test/utils/decorators/memoize.ts
@@ -0,0 +1,35 @@
+import { equal } from 'node:assert'
+import { afterEach, describe, mock, test } from 'node:test'
+import { memoized } from '../../../src/utils'
+
+const mockFn = mock.fn()
+
+class Test {
+  @memoized public square(num: number) {
+    mockFn()
+    return num ** 2
+  }
+}
+
+describe(import.meta.filename, () => {
+  afterEach(() => {
+    mockFn.mock.resetCalls()
+  })
+
+  test('should memoize results', () => {
+    const instance = new Test()
+    for (let i = 0; i < 10; i++) {
+      equal(instance.square(2), 2 ** 2)
+    }
+    equal(mockFn.mock.callCount(), 1)
+  })
+
+  test('should not memoize call with different args', () => {
+    const instance = new Test()
+    const numbers = [3, 4]
+    numbers.forEach(num => {
+      equal(instance.square(num), num ** 2)
+    })
+    equal(mockFn.mock.callCount(), numbers.length)
+  })
+})
--- scripts/svgs/css.ts
+++
@@ -1,6 +0,0 @@
-import type { CSSProperties } from 'react'
-
-export const full: CSSProperties = {
-  width: '100%',
-  height: '100%',
-}
